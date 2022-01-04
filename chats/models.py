f9f75632b6ffbc195b615bda60c84db167d20b90
ChungHoon <chad6027@gmail.com>
2022-01-04 14:06:56 +0900

    Update Chats & Member model

--- chats/models.py
+++ chats/models.py
@@ -1,3 +1,12 @@
+from django.contrib.auth import get_user_model
 from django.db import models
+from rooms.models import Room
 
-# Create your models here.
+
+class Chat(models.Model):
+    id = models.AutoField(primary_key=True)
+    writer = models.ForeignKey(to=get_user_model(), related_name='writer', on_delete=models.CASCADE)
+    message = models.TextField(null=False)
+    send_time = models.DateTimeField(auto_now=True)
+
+    room_id = models.ForeignKey(to=Room, related_name='room', on_delete=models.CASCADE)
--- rooms/models.py
+++ rooms/models.py
@@ -1,4 +1,6 @@
+from django.contrib.auth import get_user_model
 from django.db import models
+
 
 class Room(models.Model):
     id = models.AutoField(primary_key=True)
@@ -6,3 +8,6 @@ class Room(models.Model):
     background_image = models.ImageField(null=True, blank=True)
 
 
+class Member(models.Model):
+    member = models.ForeignKey(primary_key=True, to=get_user_model(), related_name="member", on_delete=models.CASCADE)
+    room = models.ForeignKey(to=Room, related_name="room", on_delete=models.CASCADE)
