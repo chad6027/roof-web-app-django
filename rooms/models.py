8777689b91bffad21c35ce379cf1fdad7d459baf
ChungHoon <chad6027@gmail.com>
2022-01-04 13:35:05 +0900

    update Room model

--- rooms/models.py
+++ rooms/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Room(models.Model):
+    id = models.AutoField(primary_key=True)
+    password = models.CharField(max_length=5)
+    background_image = models.ImageField(null=True, blank=True)
+
+
