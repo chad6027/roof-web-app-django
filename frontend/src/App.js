158c99759bea24b6b5b51b531ed1bacb8e02fa21
ChungHoon <chad6027@gmail.com>
2022-01-14 14:07:54 +0900

    Modify Room's url (frontend)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
-import {BrowserRouter as Router, Routes, Route, Link, Switch} from 'react-router-dom';
+import {BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Room from './routes/room'
 
 function App() {
   return (
           <Router>
               <Routes>
-                <Route exact={true} path="/" element={<Room />} />
+                <Route path="/room" element={<Room />} />
               </Routes>
           </Router>
   )
